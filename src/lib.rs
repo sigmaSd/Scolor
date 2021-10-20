f7293f196e302129d97a8c144988d86d1b299485
Nbiba Bedis <bedisnbiba@gmail.com>
2021-10-20 10:41:59 +0100

    Add background colors and styles

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,35 @@
 //! Simple ansi colors
 //! ```rust,no_run
-//! use scolor::Color;
-//! println!("{}", "hello".red());
-//! println!("{}", "world".green());
+//! use scolor::{Color, ColorExt};
+//!
+//! println!("{}", "hello".red().bold().underline());
+//! println!("{}", "world".green().red_bg().italic());
 //! println!("{}", "!".rgb(123,12,50));
 //! ```
+// Credits to https://stackoverflow.com/a/33206814
 use std::fmt::Display;
 
 pub trait Color {
     fn rgb(&self, r: u8, g: u8, b: u8) -> ColorString;
-    fn red(&self) -> ColorString;
-    fn green(&self) -> ColorString;
-    fn yellow(&self) -> ColorString;
-    fn blue(&self) -> ColorString;
-    fn light_blue(&self) -> ColorString;
+    fn rgb_bg(&self, r: u8, g: u8, b: u8) -> ColorString;
+    fn style(&self, effect: Effect) -> ColorString;
 }
 
-macro_rules! cs {
-    ($string:expr, $r:expr,$g:expr,$b:expr) => {
-        ColorString {
-            string: $string,
-            color: Rgb($r, $g, $b),
-        }
-    };
+pub trait ColorExt {
+    fn red(&self) -> ColorString;
+    fn red_bg(&self) -> ColorString;
+    fn green(&self) -> ColorString;
+    fn green_bg(&self) -> ColorString;
+    fn yellow(&self) -> ColorString;
+    fn yellow_bg(&self) -> ColorString;
+    fn blue(&self) -> ColorString;
+    fn blue_bg(&self) -> ColorString;
+    fn light_blue(&self) -> ColorString;
+    fn light_blue_bg(&self) -> ColorString;
+    fn italic(&self) -> ColorString;
+    fn bold(&self) -> ColorString;
+    fn underline(&self) -> ColorString;
+    fn crossed_out(&self) -> ColorString;
 }
 
 impl<T> Color for T
@@ -30,49 +37,129 @@ where
     T: Display,
 {
     fn rgb(&self, r: u8, g: u8, b: u8) -> ColorString {
-        cs!(self, r, g, b)
+        ColorString {
+            string: self,
+            color: Box::new(FG(r, g, b)),
+        }
     }
+
+    fn rgb_bg(&self, r: u8, g: u8, b: u8) -> ColorString {
+        ColorString {
+            string: self,
+            color: Box::new(BG(r, g, b)),
+        }
+    }
+
+    fn style(&self, effect: Effect) -> ColorString {
+        ColorString {
+            string: self,
+            color: Box::new(effect),
+        }
+    }
+}
+
+impl<T> ColorExt for T
+where
+    T: Color,
+{
     fn red(&self) -> ColorString {
-        cs!(self, 255, 0, 0)
+        self.rgb(255, 0, 0)
+    }
+    fn red_bg(&self) -> ColorString {
+        self.rgb_bg(255, 0, 0)
     }
     fn green(&self) -> ColorString {
-        cs!(self, 0, 255, 0)
+        self.rgb(0, 255, 0)
+    }
+    fn green_bg(&self) -> ColorString {
+        self.rgb_bg(0, 255, 0)
     }
     fn yellow(&self) -> ColorString {
-        cs!(self, 255, 255, 0)
+        self.rgb(255, 255, 0)
+    }
+    fn yellow_bg(&self) -> ColorString {
+        self.rgb_bg(255, 255, 0)
     }
     fn blue(&self) -> ColorString {
-        cs!(self, 0, 0, 255)
+        self.rgb(0, 0, 255)
+    }
+    fn blue_bg(&self) -> ColorString {
+        self.rgb_bg(0, 0, 255)
     }
     fn light_blue(&self) -> ColorString {
-        cs!(self, 0, 150, 255)
+        self.rgb(0, 150, 255)
+    }
+    fn light_blue_bg(&self) -> ColorString {
+        self.rgb_bg(0, 150, 255)
+    }
+    fn italic(&self) -> ColorString {
+        self.style(Effect::Italic)
+    }
+    fn bold(&self) -> ColorString {
+        self.style(Effect::Bold)
+    }
+    fn underline(&self) -> ColorString {
+        self.style(Effect::Underline)
+    }
+    fn crossed_out(&self) -> ColorString {
+        self.style(Effect::CrossedOut)
+    }
+}
+
+pub enum Effect {
+    Bold,
+    Faint,
+    Italic,
+    Underline,
+    ReverseVideo,
+    CrossedOut,
+}
+
+impl std::fmt::Display for Effect {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Effect::Bold => write!(f, "1"),
+            Effect::Faint => write!(f, "2"),
+            Effect::Italic => write!(f, "3"),
+            Effect::Underline => write!(f, "4"),
+            Effect::ReverseVideo => write!(f, "7"),
+            Effect::CrossedOut => write!(f, "9"),
+        }
     }
 }
 
 #[doc(hidden)]
 pub struct ColorString<'a> {
     string: &'a dyn Display,
-    color: Rgb,
+    color: Box<dyn Display>,
 }
 
 impl Display for ColorString<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        const DEL1: &str = "\x1b[";
-        const DEL2: &str = "m";
-        const DEL3: &str = "\x1b[0m";
-        const FG: &str = "38";
-        write!(
-            f,
-            "{}{};2;{}{}{}{}",
-            DEL1, FG, self.color, DEL2, self.string, DEL3
-        )
+        const START_DEL: &str = "\x1b[";
+        const COLOR_END_DEL: &str = "m";
+        const END_DEL: &str = "\x1b[0m";
+        write!(f, "{}", START_DEL)?;
+        write!(f, "{}", self.color)?;
+        write!(f, "{}", COLOR_END_DEL)?;
+        write!(f, "{}", self.string)?;
+        write!(f, "{}", END_DEL)
     }
 }
 
-struct Rgb(u8, u8, u8);
+struct FG(u8, u8, u8);
 
-impl std::fmt::Display for Rgb {
+impl std::fmt::Display for FG {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{};{};{}", self.0, self.1, self.2)
+        const FG_DEL: &str = "38;2";
+        write!(f, "{};{};{};{}", FG_DEL, self.0, self.1, self.2)
+    }
+}
+struct BG(u8, u8, u8);
+
+impl std::fmt::Display for BG {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        const BG_DEL: &str = "48;2";
+        write!(f, "{};{};{};{}", BG_DEL, self.0, self.1, self.2)
     }
 }
